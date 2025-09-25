1e820c0e0ce6957f5b8b3507d1044853aa50db04
longdao1907 <daovietkimlong@gmail.com>
2025-09-25 09:50:13 +0700

    add credential to create connection to secret manager

--- ImageAPI/Program.cs
+++ ImageAPI/Program.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Google.Apis.Auth.OAuth2;
 using Google.Cloud.SecretManager.V1;
 using ImageAPI;
 using ImageAPI.Core.Application.Interfaces;
@@ -9,43 +10,21 @@ using ImageAPI.Infrastructure.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure EF Core with PostgreSQL
-string projectId = builder.Configuration.GetSection("Gcp").GetValue<string>("ProjectID") ?? throw new ArgumentNullException("Gcp Project ID not configured.");
-string secretId = builder.Configuration.GetSection("Gcp").GetValue<string>("SecretID") ?? throw new ArgumentNullException("Gcp Secret ID not configured.");
-string jwtSecret = builder.Configuration.GetSection("Gcp").GetValue<string>("JWTSecretKey") ?? throw new ArgumentNullException("Gcp JWTSecretKey not configured.");
-string secretVersion = builder.Configuration.GetSection("Gcp").GetValue<string>("SecretVersion") ?? throw new ArgumentNullException("Gcp Secret Version not configured.");
+var credentialJson = GetGoogleCredentials();
+var credential = GoogleCredential.FromJson(credentialJson);
+SecretManagerServiceClient client = new SecretManagerServiceClientBuilder { Credential = credential }.Build();
 
-//Init Secret Manager Client
-SecretManagerServiceClient client = await SecretManagerServiceClient.CreateAsync();
-AccessSecretVersionResponse result = new AccessSecretVersionResponse();
-//get the secret value for database connection
-
-SecretVersionName secretVersionName = new(projectId, secretId, secretVersion);
-try
-{
-    Console.WriteLine($"Attempting to access secret: '{secretId}' in project: '{projectId}' version: '{secretVersion}'");
-    result = await client.AccessSecretVersionAsync(secretVersionName);
-}
-catch (Exception ex)
-{
-    Console.Error.WriteLine($"Error accessing secret: {ex.Message}");
-    throw;
-}
-
-string certContent = result.Payload.Data.ToStringUtf8();
-
-string tempCertPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.crt");
+var tempCertPath = GetTempPath();
+var certContent = GetCertContent(client);
+string jwtKey = GetJwtKey(client);
 await File.WriteAllTextAsync(tempCertPath, certContent);
-
-//get the secret value for JWT
-result = await client.AccessSecretVersionAsync(new SecretVersionName(projectId, jwtSecret, secretVersion));
-string jwtKey = result.Payload.Data.ToStringUtf8();
 
 
 //Prepare database connection string
@@ -140,6 +119,51 @@ app.MapControllers();
 app.MapGet("/health", () => "OK");
 ApplyMigrations();
 app.Run();
+
+string GetTempPath()
+{
+    string tempCertPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.crt");
+    return tempCertPath;
+}
+
+string GetJwtKey(SecretManagerServiceClient client)
+{
+    //get the secret value for JWT
+    string projectId = builder.Configuration.GetSection("Gcp").GetValue<string>("ProjectID") ?? throw new ArgumentNullException("Gcp Project ID not configured.");
+    string jwtSecret = builder.Configuration.GetSection("Gcp").GetValue<string>("JWTSecretKey") ?? throw new ArgumentNullException("Gcp JWTSecretKey not configured.");
+    string secretVersion = builder.Configuration.GetSection("Gcp").GetValue<string>("SecretVersion") ?? throw new ArgumentNullException("Gcp Secret Version not configured.");
+    AccessSecretVersionResponse result = client.AccessSecretVersion(new SecretVersionName(projectId, jwtSecret, secretVersion));
+    string jwtKey = result.Payload.Data.ToStringUtf8();
+    return jwtKey;
+}
+
+string GetGoogleCredentials()
+{
+    string projectId = builder.Configuration.GetSection("Gcp").GetValue<string>("ProjectID") ?? throw new ArgumentNullException("Gcp Project ID not configured.");
+    string gcpCredentialsSecret = builder.Configuration.GetSection("Gcp").GetValue<string>("SACredentialsKey") ?? throw new ArgumentNullException("Gcp SACredentialsKey not configured.");
+    string secretVersion = builder.Configuration.GetSection("Gcp").GetValue<string>("SecretVersion") ?? throw new ArgumentNullException("Gcp Secret Version not configured.");
+
+    //Init Secret Manager Client
+    SecretManagerServiceClient client = SecretManagerServiceClient.Create();
+
+    AccessSecretVersionResponse result = client.AccessSecretVersion(new SecretVersionName(projectId, gcpCredentialsSecret, secretVersion));
+    string gcpCredentials = result.Payload.Data.ToStringUtf8();
+    return gcpCredentials;
+}
+
+string GetCertContent(SecretManagerServiceClient client)
+{
+    // Configure EF Core with PostgreSQL
+    string projectId = builder.Configuration.GetSection("Gcp").GetValue<string>("ProjectID") ?? throw new ArgumentNullException("Gcp Project ID not configured.");
+    string secretId = builder.Configuration.GetSection("Gcp").GetValue<string>("SecretID") ?? throw new ArgumentNullException("Gcp Secret ID not configured.");
+    string secretVersion = builder.Configuration.GetSection("Gcp").GetValue<string>("SecretVersion") ?? throw new ArgumentNullException("Gcp Secret Version not configured.");
+
+    //get the secret value for database connection
+    SecretVersionName secretVersionName = new(projectId, secretId, secretVersion);
+    AccessSecretVersionResponse result = client.AccessSecretVersion(secretVersionName);
+    string certContent = result.Payload.Data.ToStringUtf8();
+    return certContent;
+}
 
 void ApplyMigrations()
 {
--- ImageAPI/WebApplicationBuilderExtension.cs
+++ ImageAPI/WebApplicationBuilderExtension.cs
@@ -38,5 +38,7 @@ namespace ImageAPI
 
             return builder;
         }
+
+
     }
 }
